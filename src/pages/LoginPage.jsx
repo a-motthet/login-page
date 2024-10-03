c3c249459277a83438bf609c2585d32d2adf7926
a-motthet <atsadawut.mott@gmail.com>
2024-10-04 00:39:25 +0700

    rename img

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaUser, FaLock } from "react-icons/fa";
-import mypic from '../images/2.jpg';
+import mypic from '../images/1.jpg';
 
 function LoginPage() {
   return (
@@ -40,7 +40,7 @@ function LoginPage() {
               <a
                 className="font-bold text-sm text-blue-500 hover:text-blue-800"
                 href="#"
-              > ลืมรหัสผ่าน ? </a>
+              > ลืมรหัสผ่าน? </a>
             </div>
             <div class="flex justify-end mt-3">
               <button
