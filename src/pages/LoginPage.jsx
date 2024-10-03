ad7ff2acccc5c1fa01720fac64912fc7e745d22a
a-motthet <atsadawut.mott@gmail.com>
2024-10-03 23:59:58 +0700

    first commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,62 +1,72 @@
 import React from 'react';
-import mypic from '../images/2.jpg'
+import { FaUser, FaLock } from "react-icons/fa";
+import mypic from '../images/2.jpg';
 
 function LoginPage() {
   return (
-          
-            <div class="bg-pupul-bg flex h-screen justify-center items-center">
-              <div class="w-1/3 bg-gradient-to-t from-puple-b to-puple-md h-4/6 rounded-l-lg"></div>
+    <div className="bg-pupul-bg flex h-screen justify-center items-center">
+      <div className="w-1/3 bg-gradient-to-t from-puple-b to-puple-md h-4/6 rounded-l-lg"></div>
 
-              <div class="w-1/3 bg-white flex items-center justify-center h-4/6 rounded-r-lg ">
-                <div class="w-full max-w-md p-8">
-                  <h2 class="text-3xl font-bold mb-6">Login</h2>
-                  <form>
-                    <div class="mb-4">
-                      <label
-                        class="block text-gray-700 text-sm font-bold mb-2"
-                        for="username"
-                      >
-                        Username
-                      </label>
-                      <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        type="text"
-                        placeholder="Enter your username"
-                      />
-                    </div>
-                    <div class="mb-6">
-                      <label
-                        class="block text-gray-700 text-sm font-bold mb-2"
-                        for="password"
-                      >
-                        Password
-                      </label>
-                      <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password"
-                        type="password"
-                        placeholder="Enter your password"
-                      />
-                    </div>
-                    <div class="flex items-center justify-between">
-                      <button
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="button"
-                      >
-                        Login
-                      </button>
-                      <a
-                        class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                        href="#"
-                      >
-                        Forgot Password?
-                      </a>
-                    </div>
-                  </form>
-                </div>
+      <div className="w-1/3 bg-white flex items-center justify-center h-4/6 rounded-r-lg">
+        <div className="w-full max-w-md p-8">
+        <div className="flex justify-center mb-7">
+            <img src={mypic} alt="Profile" className="w-64 h-64 rounded-full object-cover" />
+        </div>
+          <form>
+            <div className="mb-3">
+              <div className="flex items-center border rounded shadow-sm font-bold">
+                <FaUser className="text-gray-500 mx-4"/>
+                <input
+                  className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                />
               </div>
             </div>
+
+            <div className="mb-3">
+              <div className="flex items-center border rounded shadow-sm font-bold">
+                <FaLock className="text-gray-500 mx-4"/>
+                <input
+                  className="w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div class="flex justify-end" >
+              <a
+                className="font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="#"
+              > ลืมรหัสผ่าน? </a>
+            </div>
+            <div class="flex justify-end mt-3">
+              <button
+                className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Login
+              </button>
+            </div>
+            <div class = "flex flex-col items-center mt-3">
+              <a>
+              - - -  - - - - - - - - - - หรือ - - - - - - - - - - - - - -
+              </a>
+              <div class = "flex justify-center mt-3" >
+              <button
+                className="bg-puple-b hover:bg-puple-md text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Register
+              </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
